104be07f745828079a2d21553abfecb7f41fe945
nandiniSarma <nandinisantosh@Nandinis-MacBook-Pro.local>
2015-08-17 22:29:24 +0100

    Technical Test Project

---
+++ TechnicalTests/TestProject/SeleniumHelper.cs
@@ -0,0 +1,106 @@
+﻿namespace TestProject
+{
+    using System;
+    using FluentAssertions;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+ 
+    class SeleniumHelper
+    {
+        public static ChromeDriver driver;
+        public static int Timeout = 240;
+        public static string URL = "http://www.wikipedia.org";
+
+        public static void CreateWebDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver("../../Drivers", options, TimeSpan.FromSeconds(Timeout));
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(Timeout));
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Url = URL;
+        }
+
+        public void SearchWikipediaPageAndValidatePageHeader(string searchString, string language)
+        {
+            var searchField = driver.FindElement(By.Id("searchInput"));
+            var languageDropdown = new SelectElement(driver.FindElement(By.Id("searchLanguage")));
+            var searchButton = driver.FindElement(By.ClassName("formBtn"));
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                throw new Exception("Invalid input for Search String");
+            }
+
+            searchField.SendKeys(searchString);
+            
+            if (string.IsNullOrEmpty(language))
+            {
+                throw new Exception("Invalid input for Language");
+            }
+
+            languageDropdown.SelectByText(language);
+            searchButton.Click();
+            WaitForPageToLoad();
+
+            ValidatePageHeader(searchString);
+        }
+
+        public void ValidatePageHeader(string pageHeader)
+        {
+            if (string.IsNullOrEmpty(pageHeader))
+            {
+                throw new Exception("Invalid input for Page Header");
+            }
+
+            var heading = driver.FindElementByClassName("firstHeading").GetAttribute("textContent");
+            heading.ToLower().Should().Contain(pageHeader.ToLower());
+        }
+
+        public void WaitForPageToLoad()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(Timeout));
+            var scriptExecutor = driver as IJavaScriptExecutor;
+            wait.Until(d =>
+            {
+                string state = (string)scriptExecutor.ExecuteScript("return document.readyState");
+                return state.ToLower() == "complete";
+            });
+        }
+
+        internal void ValidateLanguage(string language)
+        {
+            var scriptExecutor = driver as IJavaScriptExecutor;
+            var displayedValue = (string) scriptExecutor.ExecuteScript("return document.documentElement.lang");
+            displayedValue.Should().Be(language);
+        }
+
+        internal void ChangeSearchResultsLanguage(string language, string pageHeader)
+        {
+            var languageLink = driver.FindElement(By.LinkText(language));
+            languageLink.Click();
+            WaitForPageToLoad();
+            ValidatePageHeader(pageHeader);
+        }
+
+        public void ValidateLanguageLinkExists(string language)
+        {
+            try
+            {
+                driver.FindElement(By.LinkText(language));
+            }
+            catch (Exception)
+            {
+                throw new Exception("Unable to find a link with language : " + language + " on search results");
+            }
+            
+        }
+
+        internal void CloseBrowser()
+        {
+           driver.Close();
+           driver.Quit();
+        }
+    }
+}
---
+++ TechnicalTests/TestProject/TechnicalTests.cs
@@ -0,0 +1,85 @@
+﻿namespace TechnicalTests
+{
+    using System;
+    using System.Xml;
+    using NUnit.Framework;
+    using TestProject.UKLocation;
+    using TestProject;
+    
+    [TestFixture]
+    public class TechnicalTests
+    {
+        [Test]
+        public void CountArrayOrderedSet()
+        {
+            var unorderedArraySet = new[] { 1, 4, 1, 4, 2, 1, 3, 5, 6, 2, 3, 7 };
+            int countOrderedSet = 0;
+            int count = 0;
+            for (int index = 0; index < unorderedArraySet.Length; index++)
+            {
+                var item = unorderedArraySet[index];
+                if ((index + 1) == unorderedArraySet.Length)
+                {
+                    break;
+                }
+
+                if (item < unorderedArraySet[index + 1])
+                {
+                    count = count + 1;
+                }
+                else
+                {
+                    countOrderedSet = countOrderedSet > count ? countOrderedSet : count;
+                    count = 0;
+                }
+            }
+
+            if (countOrderedSet == 0)
+            {
+                Console.WriteLine("No Ordered Set found in the Array Input provided");
+            }
+            else
+            {
+                Console.WriteLine("Longest length is: {0}", countOrderedSet + 1);
+            }
+        }
+
+        [Test]
+        public void LocationValidator()
+        {
+            string county = "Lancashire";
+            string location = "Earby";
+            ValidateLocationExistsInCounty(county, location);
+        }
+
+        [Test]
+        public void WikipediaLanguageSearch()
+        {
+            var helper = new SeleniumHelper();
+            SeleniumHelper.CreateWebDriver();
+            helper.SearchWikipediaPageAndValidatePageHeader("Test", "English");
+            helper.ValidateLanguage("en");
+            helper.ChangeSearchResultsLanguage("Deutsch", "Test");
+            helper.ValidateLanguage("de");
+            helper.ValidateLanguageLinkExists("English");
+            helper.CloseBrowser();
+        }
+
+        private void ValidateLocationExistsInCounty(string county, string location)
+        {
+            var soapClient = new UKLocationSoapClient();
+            var returnValue = soapClient.GetUKLocationByCounty(county);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(returnValue);
+            var outputNode = doc.SelectSingleNode(".//Table/Town[text() = '" + location + "']");
+            if (outputNode == null)
+            {
+                throw new Exception(string.Format("Expected Location : {0} not found in County {1}", location, county));
+            }
+
+            var postcode = doc.SelectSingleNode(".//Table/Town[text() = '" + location + "']/following-sibling::PostCode");
+            Console.WriteLine("Location {0} with PostCode {1} found in the County {2}", location, postcode.InnerText, county);
+        }
+
+    }
+}
